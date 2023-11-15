5e49d6e8f2a024a28b5e4be10c05d7eb07afdb61
HoodRyan <bty0823@naver.com>
2023-11-15 15:58:53 +0900

    2.5 쿼리 메소드

--- src/main/java/com/shop/repository/ItemRepository.java
+++ src/main/java/com/shop/repository/ItemRepository.java
@@ -3,6 +3,16 @@ package com.shop.repository;
 import com.shop.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ItemRepository extends JpaRepository<Item, Long> {
+
+    List<Item> findByItemNm(String itemNm); // 쿼리 메소드를 이용한 상품 조회하기
+
+    List<Item> findByItemNmOrItemDetail(String itemNm, String itemDetail); // OR 조건 처리하기
+
+    List<Item> findByPriceLessThan(Integer price);  // LessThan 조건 처리하기
+
+    List<Item> findByPriceLessThanOrderByPriceDesc(Integer price); // OrderBy로 정렬 처리하기
 
 }
