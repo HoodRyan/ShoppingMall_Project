3f1a1f88ebddf29d3b3fa66daa20ce6d31e397fa
HoodRyan <bty0823@naver.com>
2023-11-16 16:18:57 +0900

    2.7 JPA QueryDsl

--- src/main/java/com/shop/repository/ItemRepository.java
+++ src/main/java/com/shop/repository/ItemRepository.java
@@ -3,11 +3,12 @@ package com.shop.repository;
 import com.shop.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
-public interface ItemRepository extends JpaRepository<Item, Long> {
+public interface ItemRepository extends JpaRepository<Item, Long> , QuerydslPredicateExecutor<Item> {
 
     List<Item> findByItemNm(String itemNm); // 쿼리 메소드를 이용한 상품 조회하기
 
@@ -22,6 +23,8 @@ public interface ItemRepository extends JpaRepository<Item, Long> {
 
     @Query(value = "select * from item i where i.item_detail like %:itemDetail% order by i.price desc", nativeQuery = true)
     List<Item> findByItemDetailByNative(@Param("itemDetail") String itemDetail); // @Query - nativeQuery 속성 예제
+
+
 
 
 }
